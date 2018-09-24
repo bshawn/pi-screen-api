6671bc99402e5951243d35b28ed103749e387cdc
bshawn <bret.shawn@gmail.com>
2018-09-23 22:34:38 -0400

    Retrieve data from ScreenRepository

--- src/Controllers/ScreensController.cs
+++ src/Controllers/ScreensController.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ScreenApi.DataAccess;
+using ScreenApi.Models;
 
 namespace ScreenApi.Controllers
 {
@@ -10,18 +11,61 @@ namespace ScreenApi.Controllers
     [ApiController]
     public class ScreensController : ControllerBase
     {
+        // TODO: Load ScreenRepository using Dependency Injection
+        // TODO: Centralize exception handling (handle known exceptions)
+        private const string DATA_FILE = "./Data/screens.json";
+        private IScreenRepository db = new ScreenRepository(DATA_FILE);
+
         // GET api/screens
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Screen>> Get(string searchTerm)
         {
-            return new string[] { "screen1", "screen2" };
+            if (!string.IsNullOrEmpty(searchTerm))
+                return FindScreens(searchTerm);
+
+            return GetAllScreens();
+        }
+
+        private ActionResult<IEnumerable<Screen>> GetAllScreens()
+        {
+            try
+            {
+                return Ok(db.GetAllScreens());
+            }
+            catch (Exception ex)
+            {
+                return base.StatusCode(500, $"Unable to process request.  Details: {ex.ToString()}");
+            }
+        }
+
+        private ActionResult<IEnumerable<Screen>> FindScreens(string searchTerm)
+        {
+            try
+            {
+                return Ok(db.FindScreens(searchTerm));
+            }
+            catch (Exception ex)
+            {
+                return base.StatusCode(500, $"Unable to process request.  Details: {ex.ToString()}");
+            }
         }
 
         // GET api/screens/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public ActionResult<Screen> Get(Guid id)
         {
-            return "value";
+            try
+            {
+                var screen = db.GetScreenById(id);
+                if (screen == null)
+                    return NotFound("The requested screen could not be found");
+
+                return Ok(screen);
+            }
+            catch (Exception ex)
+            {
+                return base.StatusCode(500, $"Unable to process request. Details: {ex.ToString()}");
+            }
         }
 
         // POST api/screens
